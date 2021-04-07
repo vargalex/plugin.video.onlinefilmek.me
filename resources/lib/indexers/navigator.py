42a5ff4c75dbc99596469e4a01fdd957d42e3be2
Gabor Varga <gavarga@npsh.hu>
2021-04-07 09:38:35 +0200

    Profile path dekódolás

--- resources/lib/indexers/navigator.py
+++ resources/lib/indexers/navigator.py
@@ -22,7 +22,7 @@
 import os,sys,re,xbmc,xbmcgui,xbmcplugin,xbmcaddon,base64,time, locale
 import urlresolver
 from resources.lib.modules import client
-from resources.lib.modules.utils import py2_encode
+from resources.lib.modules.utils import py2_encode, py2_decode
 
 if sys.version_info[0] == 3:
     from urllib.parse import quote_plus
@@ -42,7 +42,7 @@ class navigator:
             locale.setlocale(locale.LC_ALL, '')
         except:
             pass
-        self.base_path = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile'))
+        self.base_path = py2_decode(xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')))
         self.searchFileName = os.path.join(self.base_path, "search.history")
         self.helper = helperStr.replace("Z", "").replace("W", "S")
 
